eb37b30f75a94e172c48141ffa7521d4b9b9cfb3
kiberygrozi <kostibust3@gmail.com>
2026-03-08 11:47:09 +0300

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,14 @@
+img:hover{
+transform:scale(1.03);
+box-shadow:0 10px 25px rgba(0,0,0,0.2);
+}
+
+footer{
+margin-top:60px;
+padding:30px;
+background:#0f172a;
+color:white;
+text-align:center;
+font-size:14px;
+border-top:2px solid #1e293b;
+}
